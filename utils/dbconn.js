e26770345ac320411c615b55a303d82a59f83117
AustrisLukas <austris.lukas@gmail.com>
2025-04-28 20:33:06 +0100

    azure deployment

--- utils/dbconn.js
+++ utils/dbconn.js
@@ -1,15 +1,21 @@
 /**
  * Initiates and exports database pool connection for use in the API.
  */
+const fs = require('fs');
+const path = require('path');
 const mysql = require('mysql2');
 const dotenv = require('dotenv').config({ path: './config.env' });
+
 
 const dbpool = mysql.createPool ({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASS,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        ca: fs.readFileSync(path.join(__dirname, '..', 'DigiCertGlobalRootCA.crt.pem'))
+    }
 });
 
 
